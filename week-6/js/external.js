d37d5f3577573af9e1cad9869a3770b66e6ab763
kat-thomas <thomaka@iu.edu>
2025-02-18 11:58:49 -0500

    made week-6

---
+++ week-6/js/external.js
@@ -0,0 +1,21 @@
+console.log("Umm... where are we?")
+
+const birthYear = 1963
+
+let myName = "Spider-Man"
+
+console.log(myName, "was born", birthYear)
+
+myName = "Peter Parker";
+
+// constant variables can't change their value
+// birthYear = 1776
+
+console.log(myName, "is a totally different guy!")
+
+console.log("My name is", myName.length, "characters long.")
+
+console.log("myName includes Pete:", myName.toUpperCase().includes("PETE"))
+
+console.log("myName has uppercase", myName !== myName.toLowerCase())
+console.log("myName has lowercase", myName !== myName.toUpperCase())
