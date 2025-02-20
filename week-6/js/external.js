8c0e673f350c92e43f295fdc38081ebae0a38757
kat-thomas <thomaka@iu.edu>
2025-02-20 11:44:49 -0500

    worked on week-6

--- week-6/js/external.js
+++ week-6/js/external.js
@@ -19,3 +19,22 @@ console.log("myName includes Pete:", myName.toUpperCase().includes("PETE"))
 
 console.log("myName has uppercase", myName !== myName.toLowerCase())
 console.log("myName has lowercase", myName !== myName.toUpperCase())
+
+const PI = 3.1415926535
+
+console.log(PI.toFixed(2))
+
+const randomNumber = Math.floor(Math.random() * 100) + 1;
+
+console.log("random number:", randomNumber)
+
+console.log(Math.PI - PI, PI.toString().length)
+
+console.log(PI, PI.toString())
+
+console.log("loose check", PI == PI.toString())
+console.log("strict check", PI === Number(PI.toString()))
+
+console.log("PI type:", typeof PI)
+
+console.log("myName type:", typeof myName)
